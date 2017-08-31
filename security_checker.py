b85e1fb4f308c1098f1ad25139137c5ba6e60db9
megangodwin <megan.godwin@my.jcu.edu.au>
2017-09-01 08:19:35 +1000

    adds second and third exercise

---
+++ security_checker.py
@@ -0,0 +1,5 @@
+usernames = ["jimbo", "giltson98", "derekf", "WhatSup", "NicolEye", "swe45", "BaseInterpreterInterface",
+             "BaseStdIn", "Command", "ExecState", "InteractiveConsole", "InterpreterInterface" "StartServer", "bob"]
+
+name = input("Enter your username: ")
+
